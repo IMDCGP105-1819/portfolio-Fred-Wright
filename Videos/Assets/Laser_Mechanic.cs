ddc5259a6718f798d7746bfbff70f2283680a19b
fred-wright <fredwright2000@live.co.uk>
2018-10-20 18:28:17 +0100

    groundwork

---
+++ Videos/Assets/Laser_Mechanic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser_Mechanic : MonoBehaviour {
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+    // this should be the shootey bit
+    void Shoot ()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+}
